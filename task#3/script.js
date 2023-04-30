c2b3f7a092de5bd7f44e80c55f5759e2e1128a20
Mikhail Starostin <starostin.mihail@mail.ru>
2023-04-30 21:18:07 +0300

    add files to homework#5 folder

---
+++ task#3/script.js
@@ -0,0 +1,41 @@
+/* Задание 3
+Дан массив products, необходимо цену каждого продукта уменьшить на 15% используя метод forEach.
+
+const products = [
+{
+id: 3,
+price: 200,
+},
+{
+id: 4,
+price: 900,
+},
+{
+id: 1,
+price: 1000,
+},
+]; */
+
+const products = [
+    {
+        id: 3,
+        price: 200,
+    },
+    {
+        id: 4,
+        price: 900,
+    },
+    {
+        id: 1,
+        price: 1000,
+    },
+];
+
+const k = 0.85;
+
+products.forEach(element => {
+    element.price = element.price * k;
+}
+);
+
+console.log(products);
